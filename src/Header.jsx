f94b6444be95d2a2582351f00af1fd7a5ac6ab63
Joshua Yee <joshyee16@gmail.com>
2024-01-26 13:02:43 -0800

    installed tailwind CSS, updated header styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,10 @@
 export function Header() {
   return (
     <header>
-      <nav>
-        <a href="/">Home</a> || <a href="/songs/index">Songs</a> || <a href="/songs/new">Add New Song</a> ||
+      <nav className="bg-gray-700 text-gray-300">
+        <a className="rounded-md p-1 hover:bg-gray-600 hover:text-white" href="/">Home</a> || 
+        <a className="rounded-md p-1 hover:bg-gray-600 hover:text-white" href="/songs/index">Songs</a> || 
+        <a className="rounded-md p-1 hover:bg-gray-600 hover:text-white" href="/songs/new">Add New Song</a> ||
       </nav>
     </header>
   )
--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -1,7 +1,7 @@
 export function WelcomePage() {
   return (
     <div>
-      <h1>Welcome To My Songs Site</h1>
+      <h1 className="text-3xl font-bold underline">Welcome To My Songs Site</h1>
       <h2>Make Your Own Playlist and Browse To Find What You Like</h2>
     </div>
   )
